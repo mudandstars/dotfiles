a91105295104519271d2328a5924c23e03544245
Paul <paul.sochiera@tetraeder.com>
2024-10-08 12:51:27 +0200

    updated plugings

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -1,3 +1,7 @@
 return {
+  { "folke/noice.nvim",            enabled = false },
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  { "nvim-neotest/neotest",        enabled = false },
+  { "akinsho/bufferline.nvim",     enabled = false },
 }
+
---
+++ nvim/lua/plugins/telescope.lua
@@ -0,0 +1,39 @@
+return {
+  'nvim-telescope/telescope.nvim',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons',
+    'nvim-telescope/telescope-live-grep-args.nvim',
+  },
+  keys = {
+    { '<leader>a', function() require("telescope.builtin").lsp_document_symbols({ symbols = { "method", "function" } }) end },
+  },
+  config = function()
+    require('telescope').setup({
+      defaults = {
+        path_display = { truncate = 1 },
+        prompt_prefix = '   ',
+        selection_caret = '  ',
+        layout_config = {
+          prompt_position = 'top',
+        },
+        preview = {
+          timeout = 200,
+        },
+        sorting_strategy = 'ascending',
+      },
+      pickers = {
+        find_files = {
+          hidden = true,
+        },
+        oldfiles = {
+          prompt_title = 'Search',
+        },
+        lsp_document_symbols = {
+          symbol_width = 55,
+        },
+      },
+    })
+  end,
+}
+
