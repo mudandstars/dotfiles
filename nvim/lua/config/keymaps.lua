45009fab9870d50de171aafbcc0301351474c19a
Paul <paul.sochiera@tetraeder.com>
2024-10-08 12:58:18 +0200

    felix keymaps and options test

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,26 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local keymap = vim.keymap
+
+keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })     -- split window vertically
+keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })   -- split window horizontally
+keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })      -- make split windows equal width & height
+keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+
+-- Close all open buffers
+keymap.set("n", "<leader>Q", ":bufdo bdelete<CR>")
+
+-- Paste replace visual selection without copying it.
+keymap.set("v", "p", '"_dP')
+
+-- Easy insertion of a trailing ; or , from insert mode.
+keymap.set("i", ";;", "<Esc>A;<Esc>")
+keymap.set("i", ",,", "<Esc>A,<Esc>")
+
+-- Select all
+keymap.set("n", "<C-a>", "gg<S-v>G")
+
+-- Reselect pasted text
+keymap.set('n', 'p', 'p`[v`]')
