9fc5f947303d8e0199df4920e21f843da4cbdae8
Paul <paul.sochiera@tetraeder.com>
2024-10-08 10:35:42 +0200

    formatting

---
+++ nvim/lua/plugins/conform.lua
@@ -0,0 +1,32 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        blade = { "blade-formatter" },
+        html = { "prettier" },
+        vue = { "prettier" },
+        lua = { "styleua " },
+      },
+      notify_on_error = true,
+      formatters = {
+        php = {
+          command = "php-cs-fixer",
+          args = {
+            "fix",
+            "$FILENAME",
+            "--config=./vendor/bin/php-cs-fixer fix.php --config=.php-cs-fixer.dist.php -v",
+          },
+          stdin = false,
+        },
+      },
+    },
+    keys = {
+      {
+        "<leader>cf",
+        ":lua require('conform').format()<CR>",
+        desc = "Format Current File",
+      },
+    },
+  },
+}
