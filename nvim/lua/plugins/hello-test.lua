fa8468ee666558a9d2d4162ec27d8dd2bfd94d0c
Paul Sochiera <paul@sochiera.com>
2026-03-30 12:33:18 +0200

    ft: use remote hello test pacakge

--- nvim/lua/plugins/hello-test.lua
+++ nvim/lua/plugins/hello-test.lua
@@ -1,6 +1,8 @@
 return {
     'felixele217/hello-test.nvim',
-    enabled = false, -- temporarily disabled: uses removed nvim-treesitter.ts_utils API
-    -- dev = true, -- Optional, marks this plugin as in development mode
-    -- dir = "~/code/plugins/hello-test.nvim", -- Local path to the plugin
+    enabled = true,
+    -- dir = "~/code/plugins/hello-test.nvim",
+    opts = {
+        root_dir_cmd = "./vendor/bin/pest",
+    },
 }
