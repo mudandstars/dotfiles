26c65ac2efd0a2269a8aa12edbe58b5ba0e62660
Paul Sochiera <paul@sochiera.com>
2026-04-02 09:39:41 +0200

    feat: make hello-test open in fullscreen

--- nvim/lua/plugins/hello-test.lua
+++ nvim/lua/plugins/hello-test.lua
@@ -2,6 +2,35 @@ return {
     'felixele217/hello-test.nvim',
     enabled = true,
     -- dir = "~/code/plugins/hello-test.nvim",
+    config = function(_, opts)
+        local hello_test = require('hello-test')
+
+        hello_test.setup(opts)
+
+        local function run_fullscreen(run_fn)
+            run_fn()
+            vim.schedule(function()
+                vim.cmd('wincmd |')
+                vim.cmd('wincmd _')
+            end)
+        end
+
+        vim.keymap.set('n', '<leader>ta', function()
+            run_fullscreen(hello_test.run_tests)
+        end)
+
+        vim.keymap.set('n', '<leader>tl', function()
+            run_fullscreen(hello_test.run_last)
+        end)
+
+        vim.keymap.set('n', '<leader>tf', function()
+            run_fullscreen(hello_test.run_file)
+        end)
+
+        vim.keymap.set('n', '<leader>tt', function()
+            run_fullscreen(hello_test.run_test_at_cursor)
+        end)
+    end,
     opts = {
         root_dir_cmd = "./vendor/bin/pest",
     },
